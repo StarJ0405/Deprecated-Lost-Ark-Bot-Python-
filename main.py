6f4bb80147f802b3372cc5837c49b1afcdb3cf32
StarJ0405 <49827908+StarJ0405@users.noreply.github.com>
2022-03-31 20:17:26 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 #from key import Token
 import random
+import os
 
+Token = os.environ.get('Token')
 intents = discord.Intents.default()
 intents.members = True
 bot=commands.Bot(command_prefix='./',intents = intents)

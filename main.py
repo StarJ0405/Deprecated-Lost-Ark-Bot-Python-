4dd72a52588b671581bd762525451acc1fa6c299
StarJ0405 <49827908+StarJ0405@users.noreply.github.com>
2022-04-14 22:13:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ async def task_loop():
                 dellist.append(timer)
             mention = ""
             msg = timer.getmsg()
-            for cached in client.cached_messages:
+            for cached in bot.client.cached_messages:
                 if cached.id == msg.id:
                     msg = cached
             print(msg.reactions)

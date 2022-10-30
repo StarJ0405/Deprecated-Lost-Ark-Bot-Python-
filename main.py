90a8b0e2f37e4c3ba84db85a047b2e5599df567e
StarJ0405 <49827908+StarJ0405@users.noreply.github.com>
2022-10-30 22:59:01 +0900

    2022-10-30-22-58
    fix

--- main.py
+++ main.py
@@ -124,7 +124,9 @@ async def task_loop():
 async def 와봐(ctx):
     if ctx.author.voice and ctx.author.voice.channel:
         channel = ctx.author.voice.channel
+        print("A")
         await channel.connect(timeout=3)
+        print("B")
     else:
     	await ctx.send("음성채널 없음")
 
